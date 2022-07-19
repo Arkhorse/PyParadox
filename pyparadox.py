21f9b34d94b9f6ac05e28cfda94fa3efec8f0f5e
Arkhorse <theillusivemman@gmail.com>
2022-07-19 20:22:20 -0300

    add more code to main module
    Add file, path to args(buildingsorting), add handling, change file handles

--- pyparadox.py
+++ pyparadox.py
@@ -93,33 +93,39 @@ def readlocalisationfile(file):
         
     return DATA
 
-def buildingsorting(category=str, number=int):
+def buildingsorting(file, path, category=str|'current', number=int):
     """Adds sorting to all building files in the given folder
         \n\tArguements::
-            \tcategory    : This will allow you to limit your alterations based on what category the building has. Also used to define what position priority that building is\n
-            \tnumber      : This is for testing only. If you see this please download a live version"""
+            \tfile          : This is the file you want to add the position sorting to
+            \tpath          : This is where the above file is at. 
+            \tcategory      : This will allow you to limit your alterations based on what category the building has. Also used to define what position priority that building is
+            \tnumber        : This is for testing only. If you see this please download a live version
+        \n\tExamples::
+            \t  buildingsorting(file='01_pop_assemble_buildings.txt', path='O:/SteamLibrary/steamapps/common/Stellaris/common/buildings', category='pop_assembly', number=100)
+    """
             
     # Handle all possible invalid calls
     if category == None or number == None:
         return print("Required arguments are not filled out")
     if type(number) is not int:
         return print(f"Arguement number is not an integer. Given arguement: {number}")
-    
+    if file == None:
+        return print("File is not specified")
+    if path == None:
+        return print("Path is not specified")
     
     cat = str(category)
     num = int(number)
-    for file, filename in os.path(stellaris['buildings']):
-        if os.path.isfile(stellaris['buildings'] + filename):
-            
-            with open(file, 'r+', encoding='utf-8') as input:
-                data = input.read()
-                if data['category'] == cat:
-                    data['position_priority'] = num
-                outputname = "output_" + filename
-                output = decode(data)
-                return output
-            
-            
+    if path == 'current':
+        path = os.getcwd()
+    os.chdir(path)
+    with open(file, 'w+', encoding='utf-8') as A:
+        data = A.read()
+        parsed = decode(data)
+        if parsed['category'] == cat:
+            parsed['position_priority'] = int(num)
+        else:
+            print(f'The requested category, {cat} is not present in the given file {file}, in path {path}')
 
 def UnitTest():
     building_test_pop_assembly = ''' 
@@ -157,8 +163,6 @@ class ParadoxOperators(Operators):
     def LessThanEqual(self, name, value):
         text = "{} <= {}".format(name, value)
         return text
-    
-    
 
 
 # with open(_FILE_, 'r+', encoding='utf-8') as file:
